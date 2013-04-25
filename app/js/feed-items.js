b4196555061fc7bd75166a2339d280c30cabcf57
Scott Rabin <scottrabin@gmail.com>
2013-04-24 22:18:57 -0700

    [Chapter 5] Implemented a "Filter" feature for Feed Aggregator

--- app/js/feed-items.js
+++ app/js/feed-items.js
@@ -2,20 +2,39 @@
 
 define(function(require) {
 	var defineComponent = require('flight/lib/component');
+	var aggregatorTemplate = require('text!tmpl/feed-aggregator.html');
 	var template = require('text!tmpl/feed-item.html');
 
 	function FeedItems() {
 		this.defaultAttrs({
-			"feedItem": ".feed"
+			"filterSelector": "select",
+			"feedList": ".feed-list",
+			"feedItem": ".feed-list .feed"
 		});
 
 		this.updateFeeds = function(event, feedData) {
+			// insert a new option into our select element, if it doesn't already exist
+			this.insertOption(feedData);
+
 			if (feedData.entries) {
-				feedData.entries.forEach(this.insertEntry, this);
+				feedData.entries.forEach(function(entry) {
+					this.insertEntry(entry, feedData);
+				}, this);
 			}
 		};
 
-		this.insertEntry = function(entry) {
+		this.insertOption = function(feed) {
+			var exists = (this.select('filterSelector').
+						  find('option[value="' + feed.link + '"]').length > 0);
+			if (!exists) {
+				$(document.createElement('option')).
+					text(feed.title).
+					val(feed.link).
+					appendTo(this.select('filterSelector'));
+			}
+		};
+
+		this.insertEntry = function(entry, feed) {
 			// detect an entry's existence by looking for a feed item
 			// with a link that points to the same place
 			var exists = (this.select('feedItem').
@@ -25,11 +44,29 @@ define(function(require) {
 					find('.title').text(entry.title).end().
 					find('.link').attr('href', entry.link).end().
 					find('.snippet').text(entry.contentSnippet).end().
-					appendTo(this.$node);
+					data('source', feed.link).
+					appendTo(this.select('feedList'));
+			}
+		};
+
+		this.filterEntries = function() {
+			var source = this.select('filterSelector').val();
+			if (source) {
+				// if a source is chosen, toggle the visibility of each one by one
+				this.select('feedItem').each(function() {
+					$(this).toggle($(this).data('source') == source)
+				});
+			} else {
+				// if no source chosen, show all
+				this.select('feedItem').show();
 			}
 		};
 
 		this.after('initialize', function() {
+			this.$node.html(aggregatorTemplate);
+			this.on('change', {
+				'filterSelector': this.filterEntries
+			});
 			this.on(document, 'dataFeedInfo', this.updateFeeds);
 		});
 	}
--- tests/feed_items_spec.js
+++ tests/feed_items_spec.js
@@ -30,12 +30,47 @@ describeComponent('feed-items', function() {
 	];
 
 	var FEED_DATA = {
-			title: "Test RSS Feed",
-			link:  "http://localhost/",
-			description: "This is a description of the RSS feed",
-			author: "Author's Name",
-			entries: FEED_ITEMS
-		};
+		title: "Test RSS Feed",
+		link:  "http://localhost/",
+		description: "This is a description of the RSS feed",
+		author: "Author's Name",
+		entries: FEED_ITEMS
+	};
+
+	var OTHER_FEED_ITEMS = [
+		{
+			title: "Other Item One",
+			link:  "http://127.0.0.1/item-1",
+			content: '<span>Item 1 Content</span>',
+			contentSnippet: "Item 1 Content",
+			publishedDate: "Mon, 15 Apr 2012 06:15:00 -0700",
+			categories: []
+		},
+		{
+			title: "Other Item Two",
+			link:  "http://127.0.0.1/item-2",
+			content: '<span>Item 2 Content</span>',
+			contentSnippet: "Item 2 Content",
+			publishedDate: "Wed, 17 Apr 2012 06:15:00 -0700",
+			categories: []
+		},
+		{
+			title: "Other Item Three",
+			link:  "http://127.0.0.1/item-3",
+			content: '<span>Item 3 Content</span>',
+			contentSnippet: "Item 3 Content",
+			publishedDate: "Fri, 19 Apr 2012 06:15:00 -0700",
+			categories: []
+		}
+	];
+
+	var OTHER_FEED_DATA = {
+		title: "Some other RSS feed",
+		link: "http://127.0.0.1/",
+		description: "This is an RSS feed that is not the other RSS feed",
+		author: "Me, Myself, and I",
+		entries: OTHER_FEED_ITEMS
+	};
 
 	beforeEach(setupComponent);
 
@@ -65,5 +100,42 @@ describeComponent('feed-items', function() {
 		expect(this.component.select('feedItem').length).toBe(3);
 		this.component.trigger('dataFeedInfo', FEED_DATA);
 		expect(this.component.select('feedItem').length).toBe(3);
+	});
+
+	it("should present the user with a way to filter the displayed entries by source", function() {
+		var options;
+
+		expect(this.component.select('filterSelector').length).toBeGreaterThan(0);
+
+		options = this.component.select('filterSelector').find('option').filter(function() {
+			return $(this).attr('value').length > 0;
+		});
+		expect(options.length).toBe(0);
+
+		this.component.trigger('dataFeedInfo', FEED_DATA);
+		options = this.component.select('filterSelector').find('option').filter(function() {
+			return $(this).val().length > 0;
+		});
+		expect(options.length).toBe(1);
+	});
+
+	it("should only display feed entries from a specific source when the filter has a value selected", function() {
+		this.component.trigger('dataFeedInfo', FEED_DATA);
+		this.component.trigger('dataFeedInfo', OTHER_FEED_DATA);
+
+		expect(this.component.select('feedItem').length).toBe(6);
+
+		var selector = this.component.select('filterSelector');
+		selector.val(selector.find('option:last').val());
+		selector.trigger('change');
+
+		this.component.select('feedItem').each(function() {
+			expect($(this).data('source')).toBeDefined();
+			if ($(this).data('source') == FEED_DATA.link) {
+				expect($(this)).not.toBeVisible();
+			} else {
+				expect($(this)).toBeVisible();
+			}
+		});
 	});
 });
